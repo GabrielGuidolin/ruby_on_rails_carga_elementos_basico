1508e25f362a0cae829d2eb4864e19d8fad790ba
[Gabriel] <[guidolingabriel102@gmail.com]>
2025-11-04 20:34:40 -0300

    Arreglo de los mensajes de error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
             flash[:notice] = "Article was created successfully."
             redirect_to article_path(@article)
         else
-            render :new
+            render :new, status: :unprocessable_entity
         end
     end
     def edit 
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
             flash[:notice] = "Article was updated successfully."
             redirect_to article_path(@article)
         else
-            render :edit
+            render :edit, status: :unprocessable_entity #El ultimo es para que no pierda los errores de validacion al renderizar la vista
         end
     end
     def destroy
